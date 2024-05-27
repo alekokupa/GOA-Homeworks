19aaee677a7838a1046c997c180c78ebf4c9de7b
alekokupa <alekokupa@gmail.com>
2024-05-27 20:43:28 +0400

    first commit

---
+++ level 02/homework/homework.py
@@ -0,0 +1,32 @@
+name = "chemi saxelia aleqsi"
+surname = "qufarashvili"
+age = "14 wlis"
+height = "160"
+#boolean (bool) tipis cvladi
+
+
+
+know_programming = True #True an False 
+#is_ugly = False #skanecase(ubralod weris stili srandartulad)
+#isUgly = False #javaskriptuli camelcase 
+print(name +" "+ surname + " "+ str(age) + " " + str(height))
+#stringi aris brjyalebshi moqceuli simboloebi
+
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(know_programming))
+
+#print(name + " " + str (age))
+#git add . git coomit git push
+#from turtle import *
+
+
+#print("aleqsi qufarashvili")
+
+#print("aleqsi qufarashvili")
+
+#print("aleqsi" + "qufarashvili")
---
+++ level 04/homework/homework.py
@@ -0,0 +1,19 @@
+thing = 1000
+Thing = 5
+print(thing * Thing)
+age = 14
+Age = 40
+print(age + 10)
+print(Age + 10)
+print("5" + "10")
+print(5 + 10)
+print(5 / 10)
+print(5 * 10)
+print(5 - 10)
+name = "aleqsi"
+age = 14
+print(name)
+print(age)
+age = 14
+Age = 40
+print(age + Age + 10 - (Age - age))
