0ab6733f8c1cae5a2c798143fd18ac0ff85b7081
alekokupa <alekokupa@gmail.com>
2024-07-12 19:31:50 +0400

    second commit

---
+++ level 08/classwork/classwork.py
@@ -0,0 +1,23 @@
+# boolean - True, False
+print(5 < 8)
+print(10 < 5)
+
+# boolean -
+
+print(4 < 8)
+
+num1 = 10
+num2 = 3
+print(num1 > num2)
+print(num1 < num2)
+
+print(5 == 5)
+print("goA" == "goa")
+
+# case sensitive
+print("G" == "g")
+
+# >=, <=
+
+print(5 >= 5)
+print(5 != 5)
