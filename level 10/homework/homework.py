fa551c71052e9627d537c3886c43d1f1020a06ef
alekokupa <alekokupa@gmail.com>
2024-07-13 16:09:58 +0400

    second commit

---
+++ level 10/homework/homework.py
@@ -0,0 +1,18 @@
+sum = 2
+for i in range(10):
+    print(sum)
+    sum = sum + i
+sum = 1
+for i in range(10):
+    print(sum)
+    sum = sum + i 
+sum = 0
+for i in range(10):
+    print(sum)
+    sum = sum + i   
+num = int(input("please enter your number (1-10): "))
+if num == 5:
+    print("you won!")
+else:
+    print("you lost!")
+        
